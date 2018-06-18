064b1c9d25681c49254cee6ddc4324a6c4a7fca8
Rob <rob@domains.co.za>
2018-06-18 10:23:03 +0200

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 		
 socket.on('connect', function() {
 	console.log('Connected to server');
-	
-	socket.emit('createMessage', {
-		from: 'Rob',
-		text: 'Hey. This is Rob'
-	});
 });
 
 socket.on('disconnect', function() {
